4a902fd488a5b36e05871ef978bfcf651a9ef966
DHipo <104331457+DHipo@users.noreply.github.com>
2024-04-08 00:06:05 -0300

    upload exercises

--- in_string.py
+++ in_string.py
@@ -1,6 +1,10 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    nombre = input("> ").lower()
 
+    for vowel in vowels:
+        print(f'Contiene {vowel}:', (vowel in nombre))
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
--- slice_simple.py
+++ slice_simple.py
@@ -2,6 +2,11 @@ def slice_simple():
     texto = "Awesome"
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+    texto = texto.lower() 
+    print(texto[:3])
+    print(texto[int(len(texto)/2)-1:int(len(texto)/2)+2])
+
+    print(f'{texto[:4]}{texto[len(texto)-3:]}')
 
 
 # Para verificar este ejercicio ejecutar el comando
